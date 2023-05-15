5bd569dd4981fadcea6595e71637cdc6f2372a34
portravedesign <66311356+portravedesign@users.noreply.github.com>
2023-05-15 16:58:05 +0530

    dfwcw
    cewcewcwe

--- js/app.js
+++ js/app.js
@@ -13,34 +13,12 @@ window.addEventListener("DOMContentLoaded",function(){
 })
 
 
-// var video = document.getElementById('video');
-// if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-//  navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-//  video.src = window.URL.createObjectURL(stream);
-//  video.play();
-//  });
-// }
-
-let All_mediaDevices=navigator.mediaDevices
-if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
-   console.log("getUserMedia() not supported.");
-   return;
+var video = document.getElementById('video');
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+ navigator.mediaDevices.getUserMedia({ video: true })
+ .then(function(stream) {
+ video.src = window.URL.createObjectURL(stream);
+ video.play();
+ });
 }
-All_mediaDevices.getUserMedia({
-   audio: true,
-   video: true
-})
-.then(function(vidStream) {
-   var video = document.getElementById('video');
-   if ("srcObject" in video) {
-      video.srcObject = vidStream;
-   } else {
-      video.src = window.URL.createObjectURL(vidStream);
-   }
-   video.onloadedmetadata = function(e) {
-      video.play();
-   };
-})
-.catch(function(e) {
-   console.log(e.name + ": " + e.message);
-});
+
