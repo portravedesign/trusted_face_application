26d0793e95b42c32e30aa890f9809c7ca85a47bf
portravedesign <66311356+portravedesign@users.noreply.github.com>
2023-05-15 16:24:05 +0530

    saffvf
    revrevrv

--- js/app.js
+++ js/app.js
@@ -21,18 +21,26 @@ window.addEventListener("DOMContentLoaded",function(){
 //  });
 // }
 
-var video = document.querySelector("#video");
-
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-    })
-    .catch(function (err0r) {
-      console.log("Something went wrong!");
-    });
+let All_mediaDevices=navigator.mediaDevices
+if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
+   console.log("getUserMedia() not supported.");
+   return;
 }
-
-// if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-//     navigator.mediaDevices.getUserMedia({video: true})
-//   }
+All_mediaDevices.getUserMedia({
+   audio: true,
+   video: true
+})
+.then(function(vidStream) {
+   var video = document.getElementById('video');
+   if ("srcObject" in video) {
+      video.srcObject = vidStream;
+   } else {
+      video.src = window.URL.createObjectURL(vidStream);
+   }
+   video.onloadedmetadata = function(e) {
+      video.play();
+   };
+})
+.catch(function(e) {
+   console.log(e.name + ": " + e.message);
+});
