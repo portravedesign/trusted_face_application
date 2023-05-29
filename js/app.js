2c290e33a3519eb273c0fe05d40b6d2cce97b6e2
portravedesign <66311356+portravedesign@users.noreply.github.com>
2023-05-29 10:06:02 +0530

    commit-3
    commit-3 added new video script

--- js/app.js
+++ js/app.js
@@ -5,20 +5,34 @@ var mainSection = document.querySelector(".main_section");
 
 
 
-window.addEventListener("DOMContentLoaded",function(){
-    var headerHeight = header.clientHeight;
-    var windowHeight = window.innerHeight;
-    var mainSectionHeight = windowHeight - headerHeight;
-    mainSection.style.minHeight = mainSectionHeight + "px";
-})
+// window.addEventListener("DOMContentLoaded",function(){
+//     var headerHeight = header.clientHeight;
+//     var windowHeight = window.innerHeight;
+//     var mainSectionHeight = windowHeight - headerHeight;
+//     mainSection.style.minHeight = mainSectionHeight + "px";
+// })
 
 
-var video = document.getElementById('video');
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
- navigator.mediaDevices.getUserMedia({ video: true })
- .then(function(stream) {
- video.src = window.URL.createObjectURL(stream);
- video.play();
- });
-}
+// var video = document.getElementById('video');
+// if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//  navigator.mediaDevices.getUserMedia({ video: true })
+//  .then(function(stream) {
+//  video.src = window.URL.createObjectURL(stream);
+//  video.play();
+//  });
+// }
+
+
+const webcamElement = document.getElementById('webcam');
+const canvasElement = document.getElementById('canvas');
+// const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement);
+
+webcam.start()
+   .then(result =>{
+      console.log("webcam started");
+   })
+   .catch(err => {
+       console.log(err);
+   });
 
